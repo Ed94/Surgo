774ea1835a4d87cf2589d570997176b2dfd6621b
Ed_ <edwardgz@gmail.com>
2024-04-12 11:55:38 -0400

    Added run shortcuts for scripts

--- scripts/editor.ps1
+++ scripts/editor.ps1
@@ -6,7 +6,7 @@ $path_root    = split-path -Parent -Path $path_scripts
 $path_ue      = join-path $path_root 'UE'
 $path_project = join-path $path_root 'Project'
 
-$ue_editor = join-path $path_ue Engine\Binaries\Win64\UnrealEditor.exe
+$ue_editor = join-path $path_ue 'Engine\Binaries\Win64\UnrealEditor.exe'
 
 $surgo_uproject = join-path $path_project 'Surgo.uproject'
 
